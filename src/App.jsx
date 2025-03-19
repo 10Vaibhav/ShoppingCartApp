678ab4ca7d020ec6b7d608358a523ea425735335
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-03-19 07:21:40 +0530

    Start of Shopping Cart App

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-orange-600 flex justify-center items-center text-white '>
+        <h1>hello ji
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
